5d33f62ca1ccc66c9dc9d0d2594da7932805a692
manhlnse02619 <manhlnse02619@fpt.edu.vn>
2015-08-25 11:52:49 -0700

    commit source code demo DIO project

---
+++ WIP/Sources/DIO_FALL15/DIO_FALL15/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DIO_FALL15.MvcApplication" Language="C#" %>
---
+++ WIP/Sources/DIO_FALL15/DIO_FALL15/Scripts_AngularJS/HomeModule.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+
+var app = angular.module("App", ["ngRoute"]);
+
+//app.factory("ShareData", function () {
+//    return { value: 0 }
+//});
+
+// Show Routing.
+app.config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
+    $routeProvider.when("/showproduct",
+        {
+            templateUrl: "/Products/ShowProducts",
+            controller: "ShowProductsController"
+        });
+    $routeProvider.when("/addProduct",
+        {
+            templateUrl: "Products/AddNewProduct",
+            controller: "AddProductController"
+        });
+    $routeProvider.when("/editProduct",
+        {
+            templateUrl: "Products/EditProduct",
+            controller: "EditProductController"
+        });
+    $routeProvider.when("/deleteProduct",
+        {
+            templateUrl: "Products/DeleteProduct",
+            controller: "DeleteProductController"
+        });
+    $routeProvider.otherwise(
+        {
+            redirectTo: "/"
+        });
+
+    //$locationProvider.html5Mode(false).hashPrefix("!");
+}]);
