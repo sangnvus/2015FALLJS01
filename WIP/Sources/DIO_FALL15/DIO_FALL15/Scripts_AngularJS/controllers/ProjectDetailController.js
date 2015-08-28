cc5fe14409be52a40798bbad11367298cf12bc23
manhlnse02619 <manhlnse02619@fpt.edu.vn>
2015-08-27 21:04:43 -0700

    update back project function

--- WIP/Sources/DIO_FALL15/DIO_FALL15/Scripts_AngularJS/controllers/ProjectDetailController.js
+++ WIP/Sources/DIO_FALL15/DIO_FALL15/Scripts_AngularJS/controllers/ProjectDetailController.js
@@ -1,14 +1,14 @@
 ﻿"use strict";
 
 app.controller('ProjectDetailController', function ($scope, $location, $routeParams, ProjectService) {
-
     var Id = $routeParams.id;
-
+    $scope.ProjectExprire = false;
     function loadProjectRecord() {
         var promiseGetProject = ProjectService.getProject(Id);
         promiseGetProject.then(
             function (result) {
                 $scope.Project = result.data;
+                CheckExpire();
             },
             function (error) {
                 $scope.error = error;
@@ -16,6 +16,27 @@ app.controller('ProjectDetailController', function ($scope, $location, $routePar
     }
 
     loadProjectRecord();
+
+    function CheckExpire() {
+        var now = new Date();
+        var deadline = new Date($scope.Project.Deadline);
+        $scope.ProjectExprire = (deadline <= now);
+    }
+
+    $scope.Back = function () {
+        var amount = $scope.BackAmount;
+        var promiseBack = ProjectService.BackProject(Id, amount);
+        promiseBack.then(
+            function (result) {
+                $scope.Project.CurrentFund = result.data;           
+                $('#exampleModal').modal('hide');
+                $scope.BackAmount = null;
+                alert("Back successfully! Thank you!");
+            },
+            function (error) {
+                $scope.error = error;
+            });
+    }
 
     //To Edit Project  
     $scope.editProject = function (id) {
