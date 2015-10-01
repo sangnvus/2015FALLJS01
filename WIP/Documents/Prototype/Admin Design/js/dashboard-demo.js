322be025502e7a9cbd41951d0d934bcf6cf76854
huy nguyen <huynmse02723@fpt.edu.vn>
2015-10-01 22:22:50 +0700

    update dashboard

--- WIP/Documents/Prototype/Admin Design/js/dashboard-demo.js
+++ WIP/Documents/Prototype/Admin Design/js/dashboard-demo.js
@@ -1,33 +1,117 @@
-// Flex Admin Demo Chart JavaScript
-
-
-// Disables line smoothing
-//Morris Line Chart
-// ID of the element in which to draw the chart.
-Morris.Line({
-element: 'morris-chart-line',
-// Chart data records -- each entry in this array corresponds to a point on
-// the chart.
-data: [
-  { d: '2015-01-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-02-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-03-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-04-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-05-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-06-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-07-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-08-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-09-01', projects: 9, success: 5, fail: 4 },
-  { d: '2015-09-26', projects: 9, success: 5, fail: 4 },
-],
-// The name of the data record attribute that contains x-visitss.
-xkey: 'd',
-// A list of names of data record attributes that contain y-visitss.
-ykeys: ['projects', 'success', 'fail'],
-// Labels for the ykeys -- will be displayed when you hover over the
-// chart.
-lineColors: ['#16a085','#f39c12', '#350035'],
-labels: ['projects', 'success', 'fail'],
-smooth: false,
-resize: true
+$(function () {
+    $('#container').highcharts({
+        chart: {
+            zoomType: 'xy'
+        },
+        title: {
+            text: 'Monthly Statistic'
+        },
+        xAxis: {
+            categories: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+        },
+        yAxis: [{ // Primary yAxis
+            labels: {
+                format: '{value} $',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                }
+            },
+            title: {
+                text: 'Money',
+                style: {
+                    color: Highcharts.getOptions().colors[1]
+                }
+            }
+        }, { // Secondary yAxis
+            title: {
+                text: 'Project',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            labels: {
+                format: '{value}',
+                style: {
+                    color: Highcharts.getOptions().colors[0]
+                }
+            },
+            opposite: true
+        }],
+        tooltip: {
+            shared: true
+        },
+        series: [{
+            type: 'column',
+            name: 'Created',
+            yAxis: 1,
+            data: [3, 2, 1, 3, 4, 3, 2, 1, 3, 4]
+        }, {
+            type: 'column',
+            name: 'Success',
+            yAxis: 1,
+            data: [2, 3, 5, 7, 6, 2, 3, 5, 7, 6]
+        }, {
+            type: 'column',
+            name: 'Fail',
+            yAxis: 1,
+            data: [4, 3, 3, 9, 3, 4, 3, 3, 9, 3]
+        }, {
+            type: 'spline',
+            name: 'Fund',
+            data: [3000, 2670, 3000, 6330, 3330, 3000, 2670, 3000, 6330, 3330],
+            tooltip: {
+                valueSuffix: ' $'
+            },
+        },  {
+            type: 'spline',
+            name: 'Profit',
+            data: [1000, 500, 150, 633, 700, 1000, 500, 150, 633, 700],
+            tooltip: {
+                valueSuffix: ' $'
+            },
+        }]
+    });
 });
+
+//----------------Morris Donut Chart -------------------- //
+// category
+Morris.Donut({
+		element: 'category-donut',
+		data: [
+		 {label: "Art", value: 42.7},
+		 {label: "Comic", value: 8.3},
+		 {label: "Game", value: 12.8},
+		 {label: "Film", value: 36.2}
+		],
+		resize: true,
+		colors: ['#16a085','#2980b9', '#f39c12', '#e74c3c'],
+		formatter: function (y) { return y + "%" ;}
+	}
+);
+// rank
+Morris.Donut({
+		element: 'rank-donut',
+		data: [
+		 {label: "A", value: 42.7},
+		 {label: "B", value: 8.3},
+		 {label: "C", value: 12.8},
+		 {label: "D", value: 36.2}
+		],
+		resize: true,
+		colors: ['#16a085','#2980b9', '#f39c12', '#e74c3c'],
+		formatter: function (y) { return y + "%" ;}
+	}
+);
+// rate
+Morris.Donut({
+		element: 'rate-donut',
+		data: [
+		 {label: "Success", value: 42},
+		 {label: "Fail", value: 58},
+		],
+		resize: true,
+		colors: ['#16a085','#2980b9'],
+		formatter: function (y) { return y + "%" ;}
+	}
+);
+	
