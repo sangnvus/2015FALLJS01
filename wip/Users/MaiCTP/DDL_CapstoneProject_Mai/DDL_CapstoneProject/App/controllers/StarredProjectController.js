4f0898c5818296794127cac8138566af8c2683da
maictpse02908 <maictpse02908@fpt.edu.vn>
2015-10-19 13:12:48 +0700

    Delete source from Mai folder

--- wip/Users/MaiCTP/DDL_CapstoneProject_Mai/DDL_CapstoneProject/App/controllers/StarredProjectController.js
+++
@@ -1,7 +0,0 @@
-﻿"use strict";
-
-app.controller('StarredProjectController', function ($scope, project) {
-  
-        $scope.ListStarredProject = project.data.Data;
-    
-});
--- wip/Users/MaiCTP/DDL_CapstoneProject_Mai/DDL_CapstoneProject/App/services/SlideSevice.js
+++
@@ -1,7 +0,0 @@
-﻿service.service("SlideService", function ($http) {
-
-    //Read all Students  
-    this.getSlides = function () {
-        return $http.get("api/SlideAPI/GetSlides");
-    };
-});
