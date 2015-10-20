75192d6bfa4e9251afd8670f263e8b5f75708164
anhddse02700 <anhddse02700@fpt.edu.vn>
2015-10-20 20:56:03 +0700

    Update EditPasswordController.js

---
+++ WIP/Sources/DDL_CapstoneProject/DDL_CapstoneProject/App/controllers/EditPasswordController.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+app.controller('EditPasswordController', function ($scope, $sce, userpublicinfo, UserService, toastr) {
+    $scope.userPass = userpublicinfo.data.Data;
+    $scope.rePassword = null;
+    $scope.trustSrc = function (src) {
+        return $sce.trustAsResourceUrl(src);
+    }
+
+    // Function check string startwith 'http'
+    $scope.checkHTTP = function (input) {
+        var lowerStr = (input + "").toLowerCase();
+        return lowerStr.indexOf('http') === 0;
+    }
+
+    $scope.submit = function () {
+        if ($) {
+            $scope.userPass.NewPassword = $.md5($scope.userPass.NewPassword);
+            $scope.userPass.NewPasswordConfirm = $.md5($scope.userPass.NewPasswordConfirm);
+            $scope.userPass.CurrentPassword = $.md5($scope.userPass.CurrentPassword);
+        }
+
+        var promisePost = UserService.changepassword($scope.userPass);
+
+        promisePost.then(
+            function (result) {
+                if (result.data.Status === "success") {
+                    toastr.success('Thay đổi mật khẩu thành công');
+                } else {
+                    toastr.error('Mật khẩu hiện tại sai');
+                }
+            })
+    }
+
+    // $scope.Error = null;
+});
