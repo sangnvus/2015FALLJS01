a9ca2c9addc829ee5a1c56439e0d649fbdc47f96
Trungvnse02967 <Trungvnse02967@fpt.edu.vn>
2015-10-26 15:12:29 +0700

    TrungVn send searchkey from _layout to search screen add multiselect for search screen

---
+++ WIP/Sources/DDL_CapstoneProject/DDL_CapstoneProject/App/controllers/SearchBarController.js
@@ -0,0 +1,8 @@
+﻿"use strict";
+
+app.controller('SearchBarController',
+    function ($scope, $location) {
+        $scope.submit = function (searchkey) {
+            $location.path('/search').search({ advance: 'true', searchkey: searchkey });
+        }
+    });
