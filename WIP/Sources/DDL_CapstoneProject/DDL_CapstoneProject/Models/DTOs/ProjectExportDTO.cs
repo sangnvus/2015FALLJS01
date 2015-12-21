50e4392ab3b1bdaf6c4ad79551265beaa8f2511c
Trungvnse02967 <Trungvnse02967@fpt.edu.vn>
2015-12-21 12:30:18 +0700

    for export project list admin

---
+++ WIP/Sources/DDL_CapstoneProject/DDL_CapstoneProject/Models/DTOs/ProjectExportDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDL_CapstoneProject.Models.DTOs
+{
+    public class ProjectExportDTO
+    {
+        public string ProjectCode { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Title { get; set; }
+        public string CurrentFunded { get; set; }
+        public string FundingGoal { get; set; }
+        public DateTime? ExpireDate { get; set; }
+        public string Status { get; set; }
+        public string IsFunded { get; set; }
+        public string IsExprired { get; set; }
+        public string NumberBacked { get; set; }
+        public string CategoryName { get; set; }
+        public string Location { get; set; }
+        public string NumberUpdate { get; set; }
+        public string CreatorFullname { get; set; }
+        public string CreatorUserName { get; set; }
+    }
+}
